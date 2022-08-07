01bef0b459674acba7ecd973402d74a37868a746
Reiszecke <alecman95@gmail.com>
2022-08-07 14:29:27 +0200

    .

--- heroku-ext-hotfix.sh
+++ heroku-ext-hotfix.sh
@@ -2,8 +2,8 @@
 # https://stackoverflow.com/a/73252801/2161301
 # Create extensions in the schema where Heroku requires them to be created
 # The plpgsql extension has already been created before this script is run
-bundle exec pg:psql -a $HEROKU_APP_NAME -c 'create extension if not exists citext schema heroku_ext'
-bundle exec pg:psql -a $HEROKU_APP_NAME -c 'create extension if not exists pg_stat_statements schema heroku_ext'
+pg:psql -a $HEROKU_APP_NAME -c 'create extension if not exists citext schema heroku_ext'
+pg:psql -a $HEROKU_APP_NAME -c 'create extension if not exists pg_stat_statements schema heroku_ext'
 
 # Remove enable_extension statements from schema.rb before loading it, since
 # even 'create extension if not exists' fails when the schema is not heroku_ext
